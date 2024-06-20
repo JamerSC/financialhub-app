03f2f62be6ae840646af802ea23905f890ecb2ee
Jamer Catalla <mr.catalla28@gmail.com>
2024-06-20 13:41:57 +0800

    First Commit

---
+++ src/main/java/com/jamersc/springboot/financialhub/FinancialhubApplication.java
@@ -0,0 +1,13 @@
+package com.jamersc.springboot.financialhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialhubApplication.class, args);
+	}
+
+}
