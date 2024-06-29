700659a60b59348b91f106370bcc32772494e14f
Jamer Catalla <mr.catalla28@gmail.com>
2024-06-29 12:07:59 +0800

    4c- Created admin dashboard side bar. Added ThymeleafConfig.java for Layout dialect.

---
+++ src/main/java/com/jamersc/springboot/financialhub/config/ThymeleafConfig.java
@@ -0,0 +1,19 @@
+package com.jamersc.springboot.financialhub.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+}
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,20 @@
-function demo() {
-    alert("HELLO WORLD!");
-}
+const toggleBtn =  document.querySelector("#toggle-btn");
+
+toggleBtn.addEventListener("click", function() {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.querySelector("#toggle-btn");
+    const sidebar = document.querySelector("#sidebar");
+
+    if (toggleBtn) {
+        toggleBtn.addEventListener("click", function() {
+        toggleBtn.addEventListener("click", function() {
+            sidebar.classList.toggle("expand");
+        });
+    }
+});*/
 
 //const toggleBtn = document.querySelector("#toggle-btn");
 //const sidebar = document.querySelector("#sidebar");
