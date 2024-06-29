19dbfec8f7c0a76265f5c22ff949b3e3b4bc506b
Jamer Catalla <mr.catalla28@gmail.com>
2024-06-29 19:33:52 +0800

    5c- Fixed admin dashboard layout & design, update js toggle function.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,29 +1,18 @@
+const toggleBtn = document.querySelector("#toggle-btn");
+const sidebar = document.querySelector("#sidebar");
+
+toggleBtn.addEventListener("click", function() {
+    if (sidebar.classList.contains("expand")) {
+        sidebar.classList.remove("expand");
+    } else {
+        sidebar.classList.add("expand");
+    }
+});
+
+/* Toggle a Class "expand" to #sidebar */
+/*
 const toggleBtn =  document.querySelector("#toggle-btn");
 
 toggleBtn.addEventListener("click", function() {
     document.querySelector("#sidebar").classList.toggle("expand");
-});
-
-/*document.addEventListener("DOMContentLoaded", function() {
-    const toggleBtn = document.querySelector("#toggle-btn");
-    const sidebar = document.querySelector("#sidebar");
-
-    if (toggleBtn) {
-        toggleBtn.addEventListener("click", function() {
-        toggleBtn.addEventListener("click", function() {
-            sidebar.classList.toggle("expand");
-        });
-    }
 });*/
-
-//const toggleBtn = document.querySelector("#toggle-btn");
-//const sidebar = document.querySelector("#sidebar");
-//
-//toggleBtn.addEventListener("click", function() {
-//    if (sidebar.classList.contains("expand")) {
-//        sidebar.classList.remove("expand");
-//    } else {
-//        sidebar.classList.add("expand");
-//    }
-//});
-
