92627bde3512469355e2eb4f17d4ae4c5f777408
Jamer Catalla <mr.catalla28@gmail.com>
2024-12-08 22:42:13 +0800

    update status enum class

--- src/main/java/com/jamersc/springboot/financialhub/model/Status.java
+++ src/main/java/com/jamersc/springboot/financialhub/model/Status.java
@@ -3,10 +3,9 @@ package com.jamersc.springboot.financialhub.model;
 import lombok.Getter;
 
 public enum Status {
-    OPEN("Open"),
-    IN_PROGRESS("In Progress"),
-    PENDING("Pending"),
-    COMPLETED("Completed"),
+
+    ACTIVE("Active"),
+    ARCHIVE("Archive"),
     CLOSED("Closed");
 
     private final String statusType;
